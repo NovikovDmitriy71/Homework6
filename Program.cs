fd6a0596673c15ed5a72125554db5b25c77a07e0
dmitrij <galactik71@mail.ru>
2023-05-31 23:14:32 +0300

    1

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿
+int Prompt(string massage)
+{
+    System.Console.Write(massage);
+    string value = Console.ReadLine();
+int result = Convert.ToInt32(value);
+return result;
+}
+
+int[] InputArray(int length)
+{
+    int []array = new int[length];
+    for (int i=0 ; i<array.Length; i++)
+    {
+        array[i] = Prompt($" input {i+1}");
+    }
+    return array;
+}
+void PrintArray(int[] array)
+{
+    for (int i=0; i<array.Length; i++)
+    {
+        Console.WriteLine($" ([{i}] = {array[i]}");
+    }
+}
+int CountNamber(int[] array)
+{
+    int count = 0;
+    for (int i=0; i<array.Length; i++)
+    {
+        if (array[i]>0)
+        {
+           count++; 
+        } 
+    }
+    return count;
+}
+int length= Prompt($" input element ");
+int [] array;
+array=InputArray(length);
+PrintArray(array);
+Console.WriteLine($" {CountNamber(array)}");
